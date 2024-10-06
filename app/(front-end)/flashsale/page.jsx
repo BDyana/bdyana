4dc8cb49e800eb28c73d2dec903a60ad1214bf4d
BDyana <168805526+BDyana@users.noreply.github.com>
2024-10-07 01:32:24 +0600

    file change

--- app/(front-end)/flashsale/page.jsx
+++ app/(front-end)/flashsale/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useEffect, useState } from 'react';
-import styles from '../../../styles/main.scss';
+import styles from '../../../styles/main';
 
 const FlashSale = () => {
   // Sale time and product data
