3ff55a8f5db1c78382dc4e5acaccbc64bcef842a
BDyana <168805526+BDyana@users.noreply.github.com>
2024-11-02 22:57:51 +0600

    delivery charge update

--- components/Checkout/StepForms/ShippingDetailsForm.jsx
+++ components/Checkout/StepForms/ShippingDetailsForm.jsx
@@ -113,8 +113,8 @@ export default function ShippingDetailsForm() {
                 <div className="flex gap-2 items-center">
                   <Truck className="w-8 h-8 ms-3 flex-shrink-0 " />
                   <div className="">
-                    <p>Delivery inside Dhaka - ৳ 70 (for 1 Kg)</p>
-                    <h4>৳ 20 Added Next Per Kg</h4>
+                    <p>Inside Dhaka - ৳ 50 (for 1 Kg)</p>
+                    <h4>৳ 15 Added Next Per Kg</h4>
                   </div>
                 </div>
                 <Circle className="w-5 h-5 ms-3 flex-shrink-0" />
@@ -136,35 +136,55 @@ export default function ShippingDetailsForm() {
                 <div className="flex gap-2 items-center">
                   <Truck className="w-8 h-8 ms-3 flex-shrink-0"/>
                   <div className="">
-                    <p>Delivery Outside Dhaka - ৳ 130 (for 1 Kg)</p>
+                    <p>Outside Dhaka - ৳ 110 (for 1 Kg)</p>
                     <h4>৳ 20 Added Next Per Kg</h4>
                   </div>
                 </div>
                 <Circle className="w-5 h-5 ms-3 flex-shrink-0" />
               </label>
             </li>
-            {/* <li>
+            <li>
               <input
                 type="radio"
-                id="hosting-big"
+                id="hosting-medium"
                 name="hosting"
                 value="90"
                 class="hidden peer"
                 onChange={(e) => setShippingCost(e.target.value)}
               />
               <label
-                htmlFor="hosting-big"
+                htmlFor="hosting-medium"
                 class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
               >
                 <div className="flex gap-2 items-center">
-                  <Truck className="w-8 h-8 ms-3 flex-shrink-0 " />
                   <div className="">
-                    <p>Delivery in 1 Day for ৳90</p>
+                    <p><b>Special Item : Oil (5 Liter) </b> (Inside Dhaka - ৳ 90)</p>
                   </div>
                 </div>
                 <Circle className="w-5 h-5 ms-3 flex-shrink-0" />
               </label>
-            </li> */}
+            </li>
+            <li>
+              <input
+                type="radio"
+                id="hosting-medium"
+                name="hosting"
+                value="150"
+                class="hidden peer"
+                onChange={(e) => setShippingCost(e.target.value)}
+              />
+              <label
+                htmlFor="hosting-medium"
+                class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+              >
+                <div className="flex gap-2 items-center">
+                  <div className="">
+                    <p><b>Special Item : Oil (5 Liter) </b> (Outside Dhaka - ৳ 150)</p>
+                  </div>
+                </div>
+                <Circle className="w-5 h-5 ms-3 flex-shrink-0" />
+              </label>
+            </li>
           </ul>
         </div>
       </div>
