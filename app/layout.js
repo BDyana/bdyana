3059e1676a6454ba2137e37b3121702e0de2bdcd
BDyana <168805526+BDyana@users.noreply.github.com>
2024-12-11 00:48:46 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-931GSYSZHB"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag(){dataLayer.push(arguments)}
         gtag('js', new Date());
       
         gtag('config', 'G-931GSYSZHB');
