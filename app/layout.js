ced1d75194f15448f281853a2d529864db8d2494
BDyana <168805526+BDyana@users.noreply.github.com>
2024-12-11 00:38:41 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -22,10 +22,15 @@ export default function RootLayout({ children }) {
           name="facebook-domain-verification"
           content="pzi59zqi0qrjrv2rbtdiflwivnynhz"
         />
-        <meta
-          name="google-site-verification"
-          content="MZrG05ej8tcAbK1d7W9RCA9JTHIG5Z8zdz_EHKMycfI"
-        />
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-931GSYSZHB"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-931GSYSZHB');
+      </script>
       </head>
       <body>
         <Providers>{children}</Providers>
